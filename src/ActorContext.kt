5a2c2d3ab5c828660ef1b8270506ecd6ce35ee44
Yahya <yahyatinani@gmail.com>
2020-08-14 23:11:39 +0100

    Add actor framework interfaces

---
+++ src/ActorContext.kt
@@ -0,0 +1,11 @@
+interface MessageProcessor<T> {
+
+    fun process(message: T, sender: Result<Actor<T>>)
+}
+
+interface ActorContext<T> {
+
+    fun behavior(): MessageProcessor<T>
+
+    fun become(behavior: MessageProcessor<T>)
+}
