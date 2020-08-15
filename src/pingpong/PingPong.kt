769243f87de12bfa97b42e4869cc2f06fb4a1c4c
Yahya <yahyatinani@gmail.com>
2020-08-15 11:54:47 +0100

    Implement Ping-Pong game using actors

---
+++ src/pingpong/PingPong.kt
@@ -0,0 +1,40 @@
+package pingpong
+
+import AbstractActor
+import Actor
+import result.Result
+
+import java.util.concurrent.Semaphore
+
+private val semaphore = Semaphore(1)
+
+val referee = object : AbstractActor<Int>("Referee") {
+    override fun onReceive(message: Int, sender: Result<Actor<Int>>) {
+        println("Game ended after $message shots")
+        semaphore.release()
+    }
+}
+
+fun player(id: String, sound: String, referee: Actor<Int>) =
+    object : AbstractActor<Int>(id) {
+        override fun onReceive(message: Int, sender: Result<Actor<Int>>) {
+            println("$sound $message")
+            Thread.sleep(300)
+            if (message >= 10) referee.tell(message, sender)
+            else sender.forEach({ _sender: Actor<Int> ->
+                _sender.tell(message + 1, self())
+            }, {
+                referee.tell(message, sender)
+            })
+        }
+    }
+
+fun main() {
+    val player1 = player("Player 1", "Ping", referee)
+    val player2 = player("Player 2", "Pong", referee)
+    val ball = 1
+
+    semaphore.acquire()
+    player1.tell(ball, Result(player2))
+    semaphore.acquire()
+}
